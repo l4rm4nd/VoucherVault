9b431c46ae6ef56a6539e23d881ecc6fe2e0c0ad
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2024-07-09 18:56:03 +0200

    Update admin.py
    improve admin area for item model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    # Specify the fields to display in the list view
+    list_display = ('name', 'type', 'issuer', 'issue_date', 'expiry_date', 'value', 'is_used', 'user')
+    
+    # Specify the fields to search by
+    search_fields = ('name', 'type', 'issuer', 'user__username')
+    
+    # Specify the filters to use in the list view
+    list_filter = ('type', 'is_used', 'issue_date', 'expiry_date', 'user')
+
+# Register the Item model with the customized ItemAdmin
+admin.site.register(Item, ItemAdmin)
