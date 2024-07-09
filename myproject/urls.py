a8e1800d6f2e21388560fa279f23a1d86fc72139
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2024-07-09 23:23:26 +0200

    adjust templates and add change password

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("myapp.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # Additional paths for password change
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 ]
