3f63f44d6f896ea96f9ae8a40e3cf6ff09b84c46
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2024-08-03 15:42:28 +0200

    fix: add timezone support via envs

--- myproject/settings.py
+++ myproject/settings.py
@@ -180,7 +180,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = os.environ.get('TZ', 'Europe/Berlin')
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
