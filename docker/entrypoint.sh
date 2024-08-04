4b8a81ec6f6bb03c9eaea6610cadcfb9c844aabe
L4RM4ND <21357789+l4rm4nd@users.noreply.github.com>
2024-08-04 17:28:51 +0200

    fix(docker): add volume rw checks in entrypoint
    fixes #24 and #19

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,46 +5,58 @@ generate_random_string() {
     LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 20
 }
 
+# Check read/write permissions on the /opt/app/database directory
+echo "[TASK] Validating volume permissions"
+if [ ! -w /opt/app/database ] || [ ! -r /opt/app/database ]; then
+    echo "[FAIL] Insufficient permissions on bind mount volume. Exiting."
+    echo "       > Ensure rw permissions for the www-data user/group."
+    exit 1
+else
+    echo "[DONE] Permissions ok. Container can read and write database."
+fi
+
 # Wait for PostgreSQL database
 if [ "$DB_ENGINE" = "postgres" ]; then
-    echo "[i] Waiting for PostgreSQL db..."
+    echo "[TASK] Waiting for PostgreSQL db..."
 
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
       sleep 0.1
     done
 
-    echo "[i] PostgreSQL started"
+    echo "[DONE] PostgreSQL database available"
 fi
 
 # Function to perform database migrations and initialization
 perform_migrations() {
-    echo "[~] Migrating changes to the database"
+    echo "[TASK] Migrating changes to the database"
     python manage.py makemigrations
     python manage.py makemigrations myapp
     python manage.py migrate
     python manage.py migrate myapp
+    echo ""
 
     if [ -z "$DB_INITIALIZED" ]; then
-        echo "[~] Creating periodic tasks for expiry notifications."
+        echo "[TASK] Creating periodic tasks for expiry notifications."
         python manage.py create_default_periodic_tasks
-        echo "------------------------------------"
+        echo "-------------------------------------------------"
         admin_password=$(generate_random_string)
-        echo "[!!] Creating admin superuser account"
+        echo "[TASK] Creating superuser account"
         echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '$admin_password')" | python manage.py shell
-        echo "[>] Randomly generated password: $admin_password"
-        echo "------------------------------------"
+        echo "[>] username: admin"
+        echo "[>] password: $admin_password"
+        echo "-------------------------------------------------"
     fi
 }
 
 # Check for prior database init
 if [ -z "$DB_ENGINE" ] || [ "$DB_ENGINE" = "sqlite3" ]; then
     if test -f "/opt/app/database/db.sqlite3"; then
-        echo "[i] SQLite3 database found. Skipping initialization."
+        echo "[INFO] SQLite3 database found. Skipping initialization."
         DB_INITIALIZED=true
     fi
 elif [ "$DB_ENGINE" = "postgres" ]; then
     if PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -c '\dt' | grep -q 'django_migrations'; then
-        echo "PostgreSQL database found. Skipping initialization."
+        echo "[INFO] PostgreSQL database found. Skipping initialization."
         DB_INITIALIZED=true
     fi
 fi
@@ -53,10 +65,10 @@ fi
 perform_migrations
 
 # Start Django-Celery-Beat
-echo "[~] Starting Celery worker and beat"
+echo "[TASK] Starting Celery worker and beat"
 celery -A myproject worker -l info --detach
 celery -A myproject beat -l info --detach --scheduler django_celery_beat.schedulers:DatabaseScheduler
 
 # Spawn the web server
-echo "[~] Spawning the application server"
+echo "[TASK] Spawning the application server"
 python manage.py runserver 0.0.0.0:8000 --insecure
