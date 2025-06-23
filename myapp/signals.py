40e0f036aa257c457d479f175622570450959144
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2025-06-23 17:00:21 +0200

    fix: correct import; fixes #87

--- myapp/signals.py
+++ myapp/signals.py
@@ -3,7 +3,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import *
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
